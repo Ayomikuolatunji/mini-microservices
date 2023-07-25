5e71aa54d4f7f9366f2dc1078524419999eca9b6
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-07-25 10:47:49 +0100

    first commit

---
+++ event-bus/index.js
@@ -0,0 +1,22 @@
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use("/events", (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Post Server running on port 4005");
+});
